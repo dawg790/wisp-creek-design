040f968642641d3d2057164aad8f7d1266402590
Nick Woodland <nick_woodland@hotmail.com>
2015-07-17 14:59:48 -0600

    Changes to Family Photo app

--- src/js/photos.js
+++ src/js/photos.js
@@ -9,102 +9,237 @@
 var albums = [
   {
     "year": "2015",
+    "url": "http://wispcreekdesign.com/blog/?p=1154",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2015/07/4_daddy.jpg",
+    "caption": "Teagan is born",
+    "filter": "2015 Family Teagan Colorado"
+  },
+  {
+    "year": "2015",
+    "url": "http://wispcreekdesign.com/blog/?p=1147",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2015/07/1_di.jpg",
+    "caption": "Destination Imagination 2015",
+    "filter": "2015 Sienna Colorado"
+  },
+  {
+    "year": "2015",
+    "url": "http://wispcreekdesign.com/blog/?p=1132",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2015/07/4_rockford.jpg",
+    "caption": "Christmas 2015",
+    "filter": "2015 Family Colorado Illinois"
+  },
+  {
+    "year": "2015",
+    "url": "http://wispcreekdesign.com/blog/?p=1086",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2015/06/18785521949_d5a0c5dc72_k.jpg",
+    "caption": "Dixie 200 2015",
+    "filter": "2015 Adventures Bikepacking Utah"
+  },
+  {
+    "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=1048",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/11/DSC_9649-1024x683.jpg",
     "caption": "Thanksgiving 2014",
-    "filter": "Family"
+    "filter": "2014 Family Ava Sienna Colorado"
   },
   {
     "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=1039",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/11/WP_20141115_16_40_03_Pro-1024x576.jpg",
     "caption": "Sienna's 5th Birthday",
-    "filter": "Family"
+    "filter": "2014 Family Sienna Colorado"
   },
   {
     "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=1029",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/11/WP_20141031_14_08_18_Pro-1024x576.jpg",
     "caption": "Halloween 2014",
-    "filter": "Family"
+    "filter": "2014 Family Sienna Ava Colorado"
   },
   {
     "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=1025",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/07/MC9.jpg",
-    "caption": "Colorado Trail Tour",
-    "filter": "Adventures"
+    "caption": "Colorado Trail 2014",
+    "filter": "2014 Adventures Bikepacking Colorado"
   },
   {
     "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=1010",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/08/WP_20140830_10_17_37_Pro.jpg",
     "caption": "Mt Guiyot - 13er",
-    "filter": "Adventures"
+    "filter": "2014 Adventures Colorado"
   },
   {
     "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=989",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/08/WP_20140823_18_28_12_Pro.jpg",
-    "caption": "Nonny Visit - August",
-    "filter": "Family"
+    "caption": "Nonny Visit 2014",
+    "filter": "2014 Family Colorado"
   },
   {
     "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=953",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/08/camp_yt-1024x576.jpg",
-    "caption": "Rockford Visit",
-    "filter": "Family"
+    "caption": "Rockford Visit 2014",
+    "filter": "2014 Family Illinois"
   },
   {
     "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=933",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/07/MC1.jpg",
-    "caption": "Monarch Crest - Rainbow Trail",
-    "filter": "Adventures"
+    "caption": "Monarch Crest",
+    "filter": "2014 Adventures Colorado"
   },
   {
     "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=854",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/07/WP_20140705_12_02_45_Pro_edit.jpg",
     "caption": "Great Sand Dunes",
-    "filter": "Family"
+    "filter": "2014 Family Camping Colorado"
   },
   {
     "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=835",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/06/graduate.jpg",
     "caption": "Sienna's Graduation Daycare",
-    "filter": "Family"
+    "filter": "2014 Sienna Colorado"
   },
   {
     "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=809",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/03/WP_20140326_11_58_11_Pro.jpg",
-    "caption": "Spring Break - Pueblo",
-    "filter": "Family"
+    "caption": "Pueblo 2014",
+    "filter": "2014 Family Camping Colorado"
   },
   {
     "year": "2014",
     "url": "http://wispcreekdesign.com/blog/?p=801",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/02/dance1.jpg",
     "caption": "Sienna Dancing Class",
-    "filter": "Family"
+    "filter": "2014 Sienna Colorado"
   },
   {
     "year": "2013",
     "url": "http://wispcreekdesign.com/blog/?p=772",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2014/01/1.jpg",
-    "caption": "Carlisle Holiday Visit",
-    "filter": "Family"
+    "caption": "Carlisle Christmas 2013",
+    "filter": "2013 Family Massachusetts"
   },
   {
     "year": "2013",
     "url": "http://wispcreekdesign.com/blog/?p=719",
     "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/12/holiday4.jpg",
     "caption": "Holiday Program Daycare",
-    "filter": "Family"
+    "filter": "2013 Family Ava Sienna Colorado"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=705",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/11/trunk_or_treat2.jpg",
+    "caption": "Halloween 2013",
+    "filter": "2013 Family Ava Sienna Colorado"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=690",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/10/1417653_10151705249489109_1884863673_o.jpg",
+    "caption": "Bean FeSSt 2013",
+    "filter": "2013 Adventures Colorado"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=656",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/10/rides.jpg",
+    "caption": "Chernobyl",
+    "filter": "2013 Travel International"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=637",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/10/Signs.jpg",
+    "caption": "Kiev, Ukraine",
+    "filter": "2013 Travel International"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=628",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/10/beer_food.jpg",
+    "caption": "Freising, Germany",
+    "filter": "2013 Travel International"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=609",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/09/4_summit.jpg",
+    "caption": "Mt Democrat - 14er",
+    "filter": "2013 Adventures 14ers Colorado"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=587",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/09/dad_girls.jpg",
+    "caption": "Breck Camping 2013",
+    "filter": "2013 Family Camping Colorado"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=411",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/07/IMG_0853.jpg",
+    "caption": "First Pop-up Trip",
+    "filter": "2013  Family Camping Colorado"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=471",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/08/georgia_pass.jpg",
+    "caption": "Colorado Trail Race 2013",
+    "filter": "2013 Adventures Bikepacking Colorado"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=284",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/05/Ava_park.jpg",
+    "caption": "Spring Time Fun",
+    "filter": "2013 Family Sienna Ava Colorado"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=249",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/04/Ava_slide3.jpg",
+    "caption": "Playground Fun",
+    "filter": "2013 Family Sienna Ava Colorado"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=148",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/04/Sienna_new_bike_Ava.jpg",
+    "caption": "Sienna's New Bike",
+    "filter": "2013 Family Sienna Ava Colorado"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=55",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/04/decorating2.jpg",
+    "caption": "Easter 2013",
+    "filter": "2013 Family Sienna Ava Colorado"
+  },
+  {
+    "year": "2013",
+    "url": "http://wispcreekdesign.com/blog/?p=23",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2013/04/daddy_ava.jpg",
+    "caption": "Pueblo 2013",
+    "filter": "2013 Family Camping Sienna Ava Colorado"
+  },
+  {
+    "year": "2012",
+    "url": "http://wispcreekdesign.com/blog/?p=1126",
+    "thumb": "http://wispcreekdesign.com/blog/wp-content/uploads/2012/08/clear_creek_rain_sm.jpg",
+    "caption": "Colorado Trail Race 2012",
+    "filter": "2012 Adventures Bikepacking Colorado"
   }
+
+
 ];
 
 var years = [
@@ -152,12 +287,13 @@ var ViewModel = function () {
 
     this.year = $('#album-year').val();
 
+
     // If All is choosen, every album will display and the message will ask to choose a year
     if (self.year === "All...") {
-      $('#message').text("Please select a year.");
+      $('#messageYear').text("Please select a year.");
     // If a year is choosen, than just display the year
     } else {
-      $('#message').text(self.year);
+      $('#messageYear').text("Year is set to: " + self.year);
     }
   };
 
@@ -174,7 +310,7 @@ var ViewModel = function () {
       // If All is choosen, than just push all the albums into our array
       if (this.year === "All...") array.push(self.albumList()[i]);
 
-      // If a year is choose, than just push those albums into the array
+      // If a year is choosen, than push those albums into the array
       if (this.year === self.albumList()[i].year) {
         array.push(self.albumList()[i]);
       }
@@ -182,6 +318,7 @@ var ViewModel = function () {
 
     // Now we set the Observable array to the matched items array.
     self.matchedItems(array);
+
   };
 
   // Function to filer the matchedItems array
@@ -189,13 +326,22 @@ var ViewModel = function () {
 
     // Scroll through each list item, if the class matches the selected filter, show it.
     // The class on the list items is bound to the matchedItems array
+    // TODO: Add multiple classes (like tags) then see if classes contain the filter. Use .hasClass()
     $('li').each(function() {
-      if (self.filter() === $(this).attr('class')) {
+      var filterTerm = self.filter();
+      if ($(this).hasClass(filterTerm)) {
         $(this).show();
       } else {
         $(this).hide();
       }
     });
+
+    // Set the filter message to show users which filter is currently selected
+    if (self.filter() === "Clear") {
+      $('#messageFilter').text("Tags have been cleared");
+    } else {
+      $('#messageFilter').text("Tag is set to: " + self.filter());
+    }
   };
 
   // On clicking the filters, run the filter Albums function above
@@ -210,6 +356,7 @@ var ViewModel = function () {
   $('#album-year').change(function() {
     self.getYear();
     self.getAlbums();
+    $('#messageFilter').text("Tags have been cleared.");
 
     console.log(self.year, self.matchedItems());
 
